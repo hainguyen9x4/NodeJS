1df682054a9ac402bfa63019e3aba956b01afddb
hainguyen9x4 <hainguyen9x4@gmail.com>
2021-08-30 22:01:58 +0700

    Post method

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs  = require('express-handlebars');
 const path  = require('path');
+const { urlencoded } = require('express');
+const { join } = require('path');
 const app = express();
-const port = 3000;
+const port = 3009;
 
 //cofig static folder
 app.use(express.static(path.join(__dirname,'public')));
@@ -14,6 +16,11 @@ app.use(morgan('combined'));
 app.engine('hbs', exphbs({extname:'.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));// set view folder
+//
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -22,6 +29,10 @@ app.get('/search', (req, res) => {
   console.log("------check the query parameter------");
   console.log(req.query);
   console.log(req.query.str);
+  res.render('search');
+});
+app.post('/search', (req, res) => {
+  console.log(req.body);// cần sử dụng app.use(express.urlencoded());
   res.render('search');
 });
 app.listen(port, () => {
